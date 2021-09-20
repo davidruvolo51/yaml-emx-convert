cac39bc6bdb65d56ccc8c39a87ad4a2cad84c09c
davidruvolo51 <davidruvolo51@gmail.com>
2021-09-20 11:30:42 +0200

    updated example

--- dev/example/index.py
+++ dev/example/index.py
@@ -2,7 +2,7 @@
 #' FILE: index.py
 #' AUTHOR: David Ruvolo
 #' CREATED: 2021-09-07
-#' MODIFIED: 2021-09-17
+#' MODIFIED: 2021-09-20
 #' PURPOSE: test conversion
 #' STATUS: working
 #' PACKAGES: NA
@@ -20,9 +20,11 @@
 # ```
 
 from emxconvert.convert import Convert
+# from src.emxconvert.convert import Convert
 
 # set paths to YAML data models
 c = Convert(files = [
+    'dev/example/mypkg.yaml',
     'dev/example/birddata.yml', 
     'dev/example/birddata_refs.yaml'
 ])
