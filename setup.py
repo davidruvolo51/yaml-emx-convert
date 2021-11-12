e9b167e378f7fcb3e38e30daef2347f1b8a756d7
davidruvolo51 <davidruvolo51@gmail.com>
2021-11-12 14:41:14 +0100

    rebuilt library

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/davidruvolo51/yaml-emx-convert'
 EMAIL = 'd.c.ruvolo@gmail.com'
 AUTHOR = 'David Ruvolo'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '0.0.1'
+VERSION = '0.0.2'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
