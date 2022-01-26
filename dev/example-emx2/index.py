b84190cec560af96de41d63494d50079b52e3a99
davidruvolo51 <davidruvolo51@gmail.com>
2022-01-26 11:30:00 +0100

    restructured examples

---
+++ dev/example-emx2/index.py
@@ -0,0 +1,28 @@
+#'////////////////////////////////////////////////////////////////////////////
+#' FILE: index.py
+#' AUTHOR: David Ruvolo
+#' CREATED: 2022-01-26
+#' MODIFIED: 2022-01-26
+#' PURPOSE: dev test script for EMX2 conversion tests
+#' STATUS: experimental
+#' PACKAGES: **see below**
+#' COMMENTS: NA
+#'////////////////////////////////////////////////////////////////////////////
+
+# install package first
+# use one of the following commands
+#
+# ```
+# tox -e build 
+# python setup.py sdist
+# python setup.py bdist_wheel
+# ```
+
+# from emxconvert.convert2 import Convert2
+from yamlemxconvert.convert2 import Convert2
+
+c = Convert2(file = 'dev/example-emx1/index.yaml')
+c.convert(includeData=False)
+
+
+c.write(name='birddata_emx2', outDir = 'dev/example/model/')
