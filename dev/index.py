de4ea05881e0f8a20f355f33a7a97e45ca606424
davidruvolo51 <davidruvolo51@gmail.com>
2022-09-12 15:48:17 +0200

    removed dev files

---
+++ dev/index.py
@@ -0,0 +1,26 @@
+#///////////////////////////////////////////////////////////////////////////////
+# FILE: index.py
+# AUTHOR: David Ruvolo
+# CREATED: 2022-09-12
+# MODIFIED: 2022-09-12
+# PURPOSE: local dev test script
+# STATUS: in.progress
+# PACKAGES: NA
+# COMMENTS: NA
+#///////////////////////////////////////////////////////////////////////////////
+
+from yamlemxconvert.convert2 import Convert2
+
+model = Convert2(file = 'tests/models/model_complex/birddata.yaml')
+model.convert(keepModelPackage=True)
+
+for row in model.model['molgenis']:
+  if row.get('refSchema') == 'birdData_refs':
+    row['refSchema'] = 'birdDataRefs'
+
+refs = Convert2(file = 'tests/models/model_complex/birddata_refs.yaml')
+refs.convert(keepModelPackage=False)
+refs.model['molgenis']
+
+refs.write(name="birdDataRefs",outDir='dev')
+model.write(name="birdData",outDir='dev')
